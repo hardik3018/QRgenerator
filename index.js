e01ae8a5189881217cade51581f49d93cf528ca0
hardik3018 <128910637+hardik3018@users.noreply.github.com>
2023-06-06 22:49:19 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,17 +2,18 @@ const wrapper = document.querySelector(".wrapper"),
 qrInput = wrapper.querySelector(".form input"),
 generateBtn = wrapper.querySelector(".form button"),
 qrImg = wrapper.querySelector(".qr-code img");
-download = wrapper.querySelector(".download a");
+
 let preValue;
 
 generateBtn.addEventListener("click", () => {
+    
     let qrValue = qrInput.value.trim();
     if(!qrValue || preValue === qrValue) return;
     preValue = qrValue;
     generateBtn.innerText = "Generating QR Code...";
     const a = `https://api.qrserver.com/v1/create-qr-code/?size=200x200&data=${qrValue}`;
     qrImg.src = a;
-    download.href = a;
+    
     qrImg.addEventListener("load", () => {
         wrapper.classList.add("active");
         generateBtn.innerText = "Generate QR Code";
